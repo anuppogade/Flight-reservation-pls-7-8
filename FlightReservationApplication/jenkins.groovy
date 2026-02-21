f3901b1ad70350ff173c8317aa7bc0132107c9fa
anuppogade <anuppogade@outlook.com>
2026-02-21 17:49:55 +0530

    Update jenkins.groovy

--- FlightReservationApplication/jenkins.groovy
+++ FlightReservationApplication/jenkins.groovy
@@ -18,9 +18,9 @@ pipeline{
             steps{
                 sh '''
                     cd FlightReservationApplication
-                    docker build -t anuppogade/flight-reservation-demo:latest .
-                    docker push anuppogade/flight-reservation-demo:latest
-                    docker rmi anuppogade/flight-reservation-demo:latest
+                    docker build -t mayurwagh/flight-reservation-demo:latest .
+                    docker push mayurwagh/flight-reservation-demo:latest
+                    docker rmi mayurwagh/flight-reservation-demo:latest
                 '''
             }
         }
